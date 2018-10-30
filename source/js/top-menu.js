05560343874447e978c2fe30e4e43f80ee38bde4
Regulyarniy Nikolay <regularniy@gmail.com>
2018-10-30 17:03:56 +1000

    Added hero and header

---
+++ source/js/top-menu.js
@@ -0,0 +1,10 @@
+(function() {
+  var topMenu = document.querySelector(".page-header"),
+    menuButton = document.querySelector(".page-header__button"),
+    navigationBlock = document.querySelector(".page-header__navigation");
+  topMenu.classList.remove("no-js");
+  menuButton.addEventListener("click", function(e) {
+    this.classList.toggle("page-header__button--close");
+    navigationBlock.classList.toggle("page-header__navigation--opened");
+  })
+})();
