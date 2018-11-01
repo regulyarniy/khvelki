5fcf9bf45a1e33cecfac8e99e34121bc3dd7a082
Regulyarniy Nikolay <regularniy@gmail.com>
2018-11-01 11:59:06 +1000

    Added new year countdown

---
+++ source/js/countdown.js
@@ -0,0 +1,38 @@
+//Countdown timer
+'use strict';
+(function() {
+  var toDate = new Date("Jan 1 2019").getTime(),
+    now = new Date().getTime(),
+    secondsToNewYear = Math.floor((toDate - now) / 1000),
+    daysElement = document.querySelector(".hero__count--days"),
+    hoursElement = document.querySelector(".hero__count--hours"),
+    minutesElement = document.querySelector(".hero__count--minutes"),
+    secondsElement = document.querySelector(".hero__count--seconds"),
+    countdown;
+
+  var timer = function(seconds) {
+    var now = Date.now(),
+      then = now + seconds * 1000;
+
+    countdown = setInterval(function() {
+      var secondsLeft = Math.round((then - Date.now()) / 1000);
+
+      if (secondsLeft <= 0) {
+        clearInterval(countdown);
+        return;
+      };
+
+      displayTimeLeft(secondsLeft);
+
+    }, 1000);
+  }
+
+  var displayTimeLeft = function(seconds) {
+    daysElement.textContent = Math.floor(seconds / 86400);
+    hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+    minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
+    secondsElement.textContent = seconds % 60 < 10 ? "0" + (seconds % 60) : seconds % 60;
+  }
+
+  timer(secondsToNewYear);
+})();
--- source/js/top-menu.js
+++ source/js/top-menu.js
@@ -1,7 +1,9 @@
+//Dropdown menu
 (function() {
   var topMenu = document.querySelector(".page-header"),
     menuButton = document.querySelector(".page-header__button"),
     navigationBlock = document.querySelector(".page-header__navigation");
+
   topMenu.classList.remove("no-js");
   menuButton.addEventListener("click", function(e) {
     this.classList.toggle("page-header__button--close");
